954d90be91baf84320dab0a3497e95c86811cb2a
Tony Hoang <ton01554@gmail.com>
2019-08-25 14:34:45 -0400

    starting some material changes

--- buff-keeper-angular/src/app/app.module.ts
+++ buff-keeper-angular/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,6 +28,8 @@ import { BuffFormComponent } from './buff-form/buff-form.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
     StoreModule.forRoot({status: reducer})
   ],
   providers: [],
